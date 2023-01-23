b8d47183fdfcdc7de041a3828b7f437da5f8e6c8
Christopher Angelo <angelo@angeloanan.xyz>
2023-01-23 20:46:54 +0700

    feat: no ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,16 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 import cloudflare from '@astrojs/cloudflare'
 import node from '@astrojs/node'
-import image from '@astrojs/image'
-import partytown from '@astrojs/partytown'
-import sitemap from '@astrojs/sitemap'
 import prefetch from '@astrojs/prefetch'
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), partytown(), sitemap(), prefetch()],
-  output: 'server',
-  adapter: cloudflare({})
+  integrations: [tailwind(), prefetch(), compress({})]
+  // output: 'server',
+  // adapter: cloudflare({
+  //   mode: 'advanced'
+  // })
   // adapter: node({
   //   mode: 'standalone'
   // })
