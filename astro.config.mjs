6f4dabb8bb62f5307ab2979299f9cafb441258ab
Christopher Angelo <angelo@angeloanan.xyz>
2023-01-23 21:03:08 +0700

    feat: remove prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 import cloudflare from '@astrojs/cloudflare'
 import node from '@astrojs/node'
-import prefetch from '@astrojs/prefetch'
 import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), prefetch(), compress({})]
+  integrations: [tailwind(), compress()]
   // output: 'server',
   // adapter: cloudflare({
   //   mode: 'advanced'
